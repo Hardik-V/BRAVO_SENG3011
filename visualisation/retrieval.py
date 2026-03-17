131481e8bfc4183279cf59977f9eff7741e48a1e
Hardik Verma <140232922+Hardik-V@users.noreply.github.com>
2026-03-17 23:03:48 +1100

    Update retrieval.py

--- visualisation/retrieval.py
+++ visualisation/retrieval.py
@@ -1,10 +1,10 @@
-import os
 import requests
 
-RETRIEVAL_API = "https://b5hxtt8xp6.execute-api.ap-southeast-2.amazonaws.com/dev/retrieve/financial"
+RETRIEVAL_API = "https://b5hxtt8xp6.execute-api.ap-southeast-2.amazonaws.com/dev/retrieve/financial"    # noqa
+
 
 def get_financial_data(ticker, start, end, api_key):
-    headers = {"x-api-key": api_key}   
+    headers = {"x-api-key": api_key}
     params = {
         "ticker": ticker,
         "from": start,
