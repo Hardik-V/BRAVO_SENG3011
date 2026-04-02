0ef4b588b6a70af3ffe050c7a5fef42ce6e966e3
TanviMultani <z5621319@ad.unsw.edu.au>
2026-04-02 17:14:09 +1100

    Add initial testing microservice setup and environment configuration
    Created base test-service directory with pytest unit test

---
+++ test-service/tests/unit/test_health_check.py
@@ -0,0 +1,8 @@
+import requests
+
+
+def test_health_check():
+    url = "[b5hxtt8xp6.execute-api.ap-southeast-2.amazonaws.com/dev](https://b5hxtt8xp6.execute-api.ap-southeast-2.amazonaws.com/dev/collect/health)"
+    r = requests.get(url)
+    assert r.status_code == 200
+    assert r.json()['status'] == "healthy"
